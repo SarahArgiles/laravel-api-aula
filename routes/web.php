163947b1eb0b2de11411db12dcdd86bc1d3bcd88
sarah <sarahdasilvaargiles@gmail.com>
2024-10-18 10:10:01 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,3 +8,10 @@ Route::get('/', function () {
         'message' => 'OK'
     ]);
 });
+Route::get('/somar', function(Request $request){
+    $soma = array_sum($request->all());
+    return response()->json([
+    'message' => 'OK',
+    'sum'=> $soma, 
+    ]);
+}); 
